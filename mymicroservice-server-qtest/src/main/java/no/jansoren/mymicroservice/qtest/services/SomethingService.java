eff74a1fad6e8ee9082011fa4958862a6728c8f5
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2016-04-19 18:54:14 +0200

    Created a quality test project

--- mymicroservice-server-qtest/src/main/java/no/jansoren/mymicroservice/qtest/services/SomethingService.java
+++ mymicroservice-server-qtest/src/main/java/no/jansoren/mymicroservice/qtest/services/SomethingService.java
@@ -3,11 +3,11 @@ package no.jansoren.mymicroservice.qtest.services;
 import java.lang.String;
 
 public class SomethingService {
-  public String getSomething() {
-    return "http://localhost:8080/something/get";
-  }
-
   public String doSomething() {
     return "http://localhost:8080/something/do";
+  }
+
+  public String getSomething() {
+    return "http://localhost:8080/something/get";
   }
 }
