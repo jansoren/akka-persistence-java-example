8dd01198e3d33828db995ca2eeca79a2ba665355
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2016-04-22 20:37:00 +0200

    Run generation of code plugin

--- mymicroservice-server-qtest/src/main/java/no/jansoren/mymicroservice/qtest/services/SomethingElseService.java
+++ mymicroservice-server-qtest/src/main/java/no/jansoren/mymicroservice/qtest/services/SomethingElseService.java
@@ -3,11 +3,11 @@ package no.jansoren.mymicroservice.qtest.services;
 import java.lang.String;
 
 public class SomethingElseService {
-  public String getSomethingElse() {
-    return "http://localhost:8080/somethingelse/get";
-  }
-
   public String doSomethingElse() {
     return "http://localhost:8080/somethingelse/do";
+  }
+
+  public String getSomethingElse() {
+    return "http://localhost:8080/somethingelse/get";
   }
 }
